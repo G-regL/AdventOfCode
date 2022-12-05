b35c58579261ed24198c79b98554ec33a51d4c3f
Greg <Greg.Linton@gmail.com>
2022-12-05 11:13:48 -0500

    2022, Day 5, part 2
    This one really kicked my ass.
    What tripped me up was the append on line 28. I had to reslice the picked creates otherwise, it would do weird things.

--- 2022/5/main.go
+++ 2022/5/main.go
@@ -12,12 +12,23 @@ import (
 var flag_testData = flag.Bool("test", false, "Use Test dataset")
 
 func MoveCrate(stacks [][]string, src int, dst int) {
-	//// take crate off source stack
+	//Identify which creates to move
 	crate := stacks[src-1][0]
+	//... and remove from source stack
 	stacks[src-1] = stacks[src-1][1:]
 
-	//put it on top of destination stack
+	//Put it on top of destination stack
 	stacks[dst-1] = append([]string{crate}, stacks[dst-1]...)
+func MoveCrates(stacks [][]string, cnt int, src int, dst int) {
+	//Identify which creates to move
+	crates := stacks[src-1][0:cnt]
+	//... and remove from source stack
+	stacks[src-1] = stacks[src-1][cnt:]
+	//Put them on top of destination stack
+	// crates is resliced here such that its capacity is now equal to its length
+	// This guarantees that a new underlying array is always created for stacks9001[dst-1]
+	// --found at https://freshman.tech/snippets/go/concatenate-slices/
+	stacks[dst-1] = append(crates[:len(crates):len(crates)], stacks[dst-1]...)
 }
 
 func main() {
@@ -40,16 +51,18 @@ func main() {
 	parts := strings.Split(sfile, "\n\n")
 
 	stackdef := strings.Split(parts[0], "\n")
-	stacks := make([][]string, len(columns))
+	stacks_CM9000 := make([][]string, len(columns))
+	stacks_CM9001 := make([][]string, len(columns))
 	moves := strings.Split(parts[1], "\n")
 
 	for _, e := range stackdef[:len(stackdef)-1] {
 		for i, c := range columns {
 			if e[c] != ' ' {
-				stacks[i] = append(stacks[i], string(e[c]))
+				stacks_CM9000[i] = append(stacks_CM9000[i], string(e[c]))
 			}
 		}
 	}
+	copy(stacks_CM9001, stacks_CM9000)
 
 	for _, move := range moves {
 		moveparts := strings.Split(move, " ")
@@ -58,19 +71,21 @@ func main() {
 		move_dst, _ := strconv.Atoi(moveparts[5])
 
 		for m := 1; m <= move_cnt; m++ {
-			MoveCrate(stacks, move_src, move_dst)
+			MoveCrate(stacks_CM9000, move_src, move_dst)
 		}
+		MoveCrates(stacks_CM9001, move_cnt, move_src, move_dst)
 	}
 
 	stacktops_1 := ""
-
-	for _, s := range stacks {
+	stacktops_2 := ""
+	for _, s := range stacks_CM9000 {
 		stacktops_1 += s[0]
 	}
-
-	// stacktops_2 := ""
+	for _, s := range stacks_CM9001 {
+		stacktops_2 += s[0]
+	}
 
 	fmt.Println("\n-----------------------")
-	fmt.Println("Stack tops for Puzzle #1:", stacktops_1)
-	//fmt.Println("Total for Puzzle #2:", total_2)
+	fmt.Println("Stack tops for CrateMover 9000:", stacktops_1)
+	fmt.Println("Stack tops for CrateMover 9001:", stacktops_2)
 }
