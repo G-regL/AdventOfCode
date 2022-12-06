d6ca498dccf91c7617bcb773f4fa37062b175a57
Greg <Greg.Linton@gmail.com>
2022-12-06 11:48:28 -0500

    Got 2022/6, 1 Gold star!

--- 2022/6/main.go
+++ 2022/6/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
@@ -21,21 +22,61 @@ func main() {
 		filename = "test.txt"
 	}
 
-	// Read in file as bytes, and convert to a string.
-	bfile, err := os.ReadFile(filename)
+	// Open file for reading
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sfile := string(bfile)
 
-	// Split the file into the stack definitions, and moves
-	parts := strings.Split(sfile, "\n\n")
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
 
-	stacktops_1 := ""
-	stacktops_2 := ""
+	startOfPacket := 0
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+		buffer := line[0:4]
+		pointer := 4
+
+		fmt.Printf("Line starting with '%s...'\n", strings.Join(line[:20], ""))
+		for {
+			if pointer >= len(line) {
+				fmt.Println("No Start-of-Packet found :(")
+				break
+			}
+			//fmt.Println("buffer:", buffer)
+			// check if buffer contains unique chars
+			chars := make(map[string]bool)
+			list := []string{}
+			for _, c := range buffer {
+				if _, value := chars[c]; !value {
+					chars[c] = true
+					list = append(list, c)
+				}
+			}
+			//fmt.Println("list:  ", list)
+
+			if len(buffer) == len(list) {
+				startOfPacket = pointer
+				fmt.Println("  Start-of-Packet found after character", startOfPacket)
+				break
+			} else {
+				//update buffer with a new char
+				//fmt.Println(pointer, len(line), pointer >= len(line))
+				// if pointer > len(line) {
+				// 	fmt.Println("No Start-of-Packet found :(")
+				// 	break
+				// } else {
+				buffer = append(buffer[1:], line[pointer])
+				pointer++
+				//}
+			}
+		}
+	}
+	//stacktops_2 := ""
 
 	// Show us what we've got!
-	fmt.Println("\n-----------------------")
-	fmt.Println("Stack tops for CrateMover 9000:", stacktops_1)
-	fmt.Println("Stack tops for CrateMover 9001:", stacktops_2)
+	//fmt.Println("\n-----------------------")
+	//fmt.Println("Start-of-Packet found after character:", startOfPacket)
+	//fmt.Println("Stack tops for CrateMover 9001:", stacktops_2)
 }
