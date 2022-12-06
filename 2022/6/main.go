382eb4c47b0b61657f4ce582b1b1c0e1e2b7ef1b
Greg <Greg.Linton@gmail.com>
2022-12-06 14:10:34 -0500

    Generalize the core logic into a function
    This was *way* faster/easier than I thought.. I think it was the wording that tripped me up.

--- 2022/6/main.go
+++ 2022/6/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
@@ -22,9 +21,26 @@ func dedup(in []string) (out []string) {
 	return
 }
 
-func main() {
-	flag.Parse()
+func find(line []string, frameSize int) (position int) {
+	position = frameSize
+	buffer := line[0:frameSize]
+	found := false
+	for !found {
+		if len(buffer) == len(dedup(buffer)) {
+			found = true
+		} else {
+			//move buffer forward 1 char, increment pointer
+			buffer = append(buffer[1:], line[position])
+			position++
+		}
+	}
 
+	return
+}
+
+func main() {
+
+	flag.Parse()
 	filename := "input.txt"
 
 	// Detect the --test flag and use test data if needed
@@ -34,58 +50,16 @@ func main() {
 	}
 
 	// Open file for reading
-	f, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	f, _ := os.Open(filename)
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
 
-	startOfPacket := map[int]int{}
-	startOfSignal := map[int]int{}
-	lineCounter := 1
+	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "")
 		fmt.Printf("Line starting with '%s...'\n", strings.Join(line[:20], ""))
 
-		pointer_p1 := 4
-		buffer_p1 := line[0:pointer_p1]
-		_, SoPExists := startOfPacket[lineCounter]
-		for !SoPExists {
-			// Part 1
-			if len(buffer_p1) == len(dedup(buffer_p1)) {
-				// capture pointer, and break out
-				startOfPacket[lineCounter] = pointer_p1
-				fmt.Println("  Start-of-Packet found after character", startOfPacket[lineCounter])
-			} else {
-				//move buffer forward 1 char, increment pointer
-				buffer_p1 = append(buffer_p1[1:], line[pointer_p1])
-				pointer_p1++
-			}
-			_, SoPExists = startOfPacket[lineCounter]
-		}
-
-		pointer_p2 := 14
-		buffer_p2 := line[0:pointer_p2]
-		_, SoSExists := startOfSignal[lineCounter]
-		for !SoSExists {
-			if len(buffer_p2) == len(dedup(buffer_p2)) {
-				// capture pointer, and break out
-				startOfSignal[lineCounter] = pointer_p2
-				fmt.Println("  Start-of-Signal found after character", startOfSignal[lineCounter])
-				break
-			} else {
-				//if pointer_p2 >= len(line) {
-				//	buffer_p2 = buffer_p2[1:]
-				//} else {
-				buffer_p2 = append(buffer_p2[1:], line[pointer_p2])
-				//}
-				pointer_p2++
-			}
-			_, SoSExists = startOfSignal[lineCounter]
-		}
-		lineCounter++
+		fmt.Println("  Start-of-Packet found after character", find(line, 4))
+		fmt.Println("  Start-of-Signal found after character", find(line, 14))
 	}
 }
