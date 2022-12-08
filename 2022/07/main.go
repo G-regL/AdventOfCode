a99e6b9946d1ea23ac75f2df57b51278891c32c2
Greg <Greg.Linton@gmail.com>
2022-12-07 21:46:09 -0500

    Clean up garbage code in 2022/07

--- 2022/07/main.go
+++ 2022/07/main.go
@@ -5,67 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 )
-
-type File struct {
-	Name string
-	Size int
-}
-
-type Folder struct {
-	Name    string
-	Files   []File
-	Folders map[string]*Folder
-	Size    int
-}
-
-func newFolder(name string) *Folder {
-	return &Folder{name, []File{}, make(map[string]*Folder), 0}
-}
-
-func (f *Folder) getFolder(name string) *Folder {
-	if nextF, ok := f.Folders[name]; ok {
-		return nextF
-	} else {
-		//log.Fatalf("Expected nested folder %v in %v\n", name, f.Name)
-		return nil
-	}
-	return &Folder{} // cannot happen
-}
-
-func (f *Folder) addFolder(path []string) {
-	for i, segment := range path {
-		if i == len(path)-1 { // last segment == new folder
-			f.Folders[segment] = newFolder(segment)
-		} else {
-			f.getFolder(segment).addFolder(path[1:])
-		}
-	}
-}
-
-func (f *Folder) addFile(path []string, size int) {
-	for i, segment := range path {
-		if i == len(path)-1 { // last segment == file
-			f.Files = append(f.Files, File{segment, size})
-		} else {
-			f.getFolder(segment).addFile(path[1:], size)
-			return
-		}
-	}
-}
-
-func (f *Folder) String() string {
-	var str string
-	for _, file := range f.Files {
-		str += f.Name + string(filepath.Separator) + file.Name + "\n"
-	}
-	for _, folder := range f.Folders {
-		str += folder.String()
-	}
-	return str
-}
 
 var flag_testData = flag.Bool("test", false, "Use Test dataset")
 
@@ -93,7 +34,6 @@ func main() {
 
 		if strings.HasPrefix(line, "$ cd") {
 			// command (cd/ls)
-			//$ cd /
 			newdir := ""
 			_, _ = fmt.Sscanf(line, "$ cd %s", &newdir)
 
@@ -102,7 +42,6 @@ func main() {
 			} else {
 				currentWorkingDir = append(currentWorkingDir, newdir)
 			}
-			//fmt.Println("changed CWD to ", currentWorkingDir)
 
 		} else {
 			//listing
@@ -113,23 +52,18 @@ func main() {
 				dirpath := strings.Join(append(currentWorkingDir, dir), "/")
 
 				if _, ok := structure[dirpath]; !ok {
-					//fmt.Println("folder", dirpath, "doesn't exist")
 					structure[dirpath] = 0
 				}
 			} else {
 				size := 0
 				file := ""
 				_, _ = fmt.Sscanf(line, "%d %s", &size, &file)
-
-				//filepath := strings.Join(append(currentWorkingDir, file), "/")
-				//structure[filepath] = size
 				for i := len(currentWorkingDir); i != 0; i-- {
 					structure[strings.Join(currentWorkingDir[:i], "/")] += size
 				}
 
 			}
 		}
-		//fmt.Println("CWD: ", currentWorkingDir)
 	}
 
 	sumP1 := 0
