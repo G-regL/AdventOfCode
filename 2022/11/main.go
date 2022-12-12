611dea6ed2894d3c3a7be88f653ef7e40184b324
Greg <Greg.Linton@gmail.com>
2022-12-11 22:14:33 -0500

    Attempt at using big.Int for 2022/11/p2
    This doesn't seem to work at all, and bungs up even P1's results, so it's a no go.

--- 2022/11/main.go
+++ 2022/11/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"sort"
 	"strconv"
@@ -14,7 +15,7 @@ import (
 var flag_testData = flag.Bool("test", false, "Use Test dataset")
 
 type Monkey struct {
-	items       []int
+	items       []*big.Int
 	opType      string
 	opNumber    int
 	testDivisor int
@@ -25,6 +26,14 @@ type Monkey struct {
 func toInt(in string) (out int) {
 	out, _ = strconv.Atoi(in)
 	return out
+}
+
+func toBigInt(in string) (out *big.Int) {
+	out, ok := new(big.Int).SetString(in, 0)
+	if ok {
+		return
+	}
+	return
 }
 
 func showMonkeys(monkeys []Monkey) {
@@ -42,17 +51,17 @@ func showMonkeys(monkeys []Monkey) {
 	}
 }
 
-func updateWorryLevel(m Monkey, item int) (wl int, text string) {
-	n := m.opNumber
-	if n == -1 {
+func updateWorryLevel(m Monkey, item *big.Int) (wl big.Int, text string) {
+	n := big.NewInt(int64(m.opNumber))
+	if n == big.NewInt(int64(-1)) {
 		n = item
 	}
 
 	if m.opType == "*" {
-		wl = item * n
+		wl.Mul(item, n)
 		text = fmt.Sprintf("    Worry level is multiplied by %d to %d.", n, wl)
 	} else if m.opType == "+" {
-		wl = item + n
+		wl.Add(item, n)
 		text = fmt.Sprintf("    Worry level increases  by %d to %d.", n, wl)
 	} else {
 		log.Fatal("Unknown operator", m.opType, "in updateWorryLevel")
@@ -84,6 +93,7 @@ func main() {
 	inputMonkeys := strings.Split(sfile, "\n\n")
 
 	Monkeys := make([]Monkey, len(inputMonkeys))
+	Monkeys_p2 := make([]Monkey, len(inputMonkeys))
 
 	for num, m := range inputMonkeys {
 		props := strings.Split(m, "\n")
@@ -96,7 +106,7 @@ func main() {
 		//_, _ = fmt.Sscanf(props[1], "  Starting items: %s", &items)
 		//fmt.Println(items)
 		for _, i := range strings.Split(items[1], ", ") {
-			Monkeys[num].items = append(Monkeys[num].items, toInt(i))
+			Monkeys[num].items = append(Monkeys[num].items, toBigInt(i))
 		}
 
 		opNumber := ""
@@ -114,13 +124,17 @@ func main() {
 
 	}
 
-	inspectedItems := make([]int, len(inputMonkeys))
+	//showMonkeys(Monkeys)
 
-	for round := 1; round <= 20; round++ {
-		fmt.Printf("----- Round %2d -----\n", round)
+	copy(Monkeys_p2, Monkeys)
+
+	inspectedItems_p1 := make([]int, len(inputMonkeys))
+
+	for round_p1 := 1; round_p1 <= 20; round_p1++ {
+		//fmt.Printf("----- Round Part 1 - %2d -----\n", round_p1)
 
 		for mID, Monkey := range Monkeys {
-			inspectedItems[mID] += len(Monkey.items)
+			inspectedItems_p1[mID] += len(Monkey.items)
 			//fmt.Printf("Monkey %d:\n", mID)
 			for _, Item := range Monkey.items {
 				//fmt.Printf("  Monkey inspects an item with a worry level of %d. \n", Item)
@@ -129,18 +143,18 @@ func main() {
 				//worryLevel, wlText := updateWorryLevel(Monkey, Item)
 				//fmt.Println(wlText)
 
-				worryLevel = worryLevel / 3
+				worryLevel.Div(&worryLevel, big.NewInt(3))
 				//fmt.Printf("    Monkey gets bored with item. Worry level is divided by 3 to %d.\n", worryLevel)
-
 				//remainder := worryLevel % 23
-				if worryLevel%Monkey.testDivisor == 0 {
+				remainder := new(big.Int).Mod(&worryLevel, big.NewInt(int64(Monkey.testDivisor)))
+				if remainder == big.NewInt(0) {
 					//fmt.Printf("    Current worry level is divisible by %d.\n", Monkey.testDivisor)
 					//fmt.Printf("    Item with worry level %d is thrown to monkey %d.\n", worryLevel, Monkey.destTrue)
-					Monkeys[Monkey.destTrue].items = append(Monkeys[Monkey.destTrue].items, worryLevel)
+					Monkeys[Monkey.destTrue].items = append(Monkeys[Monkey.destTrue].items, &worryLevel)
 				} else {
 					//fmt.Printf("    Current worry level is not divisible by %d.\n", Monkey.testDivisor)
 					//fmt.Printf("    Item with worry level %d is thrown to monkey %d.\n", worryLevel, Monkey.destFalse)
-					Monkeys[Monkey.destFalse].items = append(Monkeys[Monkey.destFalse].items, worryLevel)
+					Monkeys[Monkey.destFalse].items = append(Monkeys[Monkey.destFalse].items, &worryLevel)
 				}
 
 				Monkeys[mID].items = Monkeys[mID].items[1:]
@@ -148,22 +162,56 @@ func main() {
 			// WORK OUT WHY IT WON'T REMOVE OLD ITEMS FROM THE LIST
 			//Monkey.items = []int{}
 		}
-		for mID, Monkey := range Monkeys {
-			fmt.Printf("Monkey %d: %d\n", mID, Monkey.items)
+
+		// for mID, Monkey := range Monkeys {
+		// 	fmt.Printf("Monkey %d: %d\n", mID, Monkey.items)
+		// }
+		if round_p1 == 1 || round_p1 == 20 {
+			fmt.Printf("----- Round Part 2 - %2d -----\n", round_p1)
+			for mID, count := range inspectedItems_p1 {
+				fmt.Printf("Monkey %d inspected items %d times.\n", mID, count)
+			}
 		}
-
 	}
+
 	fmt.Println("\n-----------------------")
-	for mID, count := range inspectedItems {
-		fmt.Printf("Monkey %d inspected items %d times.\n", mID, count)
+
+	inspectedItems_p2 := make([]int, len(inputMonkeys))
+	for round_p2 := 1; round_p2 <= 2000; round_p2++ {
+		for mID, Monkey := range Monkeys_p2 {
+			inspectedItems_p2[mID] += len(Monkey.items)
+			for _, Item := range Monkey.items {
+
+				worryLevel, _ := updateWorryLevel(Monkey, Item)
+				//worryLevel, wlText := updateWorryLevel(Monkey, Item)
+				//fmt.Println(wlText)
+
+				//worryLevel = worryLevel
+
+				remainder := new(big.Int).Mod(&worryLevel, big.NewInt(int64(Monkey.testDivisor)))
+				if remainder == big.NewInt(0) {
+					Monkeys_p2[Monkey.destTrue].items = append(Monkeys_p2[Monkey.destTrue].items, &worryLevel)
+				} else {
+					Monkeys_p2[Monkey.destFalse].items = append(Monkeys_p2[Monkey.destFalse].items, &worryLevel)
+				}
+
+				Monkeys_p2[mID].items = Monkeys_p2[mID].items[1:]
+			}
+		}
+		if round_p2 == 1 || round_p2 == 20 || round_p2%1000 == 0 {
+			fmt.Printf("----- Round Part 2 - %2d -----\n", round_p2)
+			for mID, count := range inspectedItems_p2 {
+				fmt.Printf("Monkey %d inspected items %d times.\n", mID, count)
+				//fmt.Println("items:", Monkeys_p2[mID].items)
+			}
+		}
 	}
 
-	sort.Ints(inspectedItems)
-
-	//showMonkeys(Monkeys)
+	sort.Ints(inspectedItems_p1)
+	sort.Ints(inspectedItems_p2)
 
 	// Show us what we've got!
 	fmt.Println("\n-----------------------")
-	fmt.Println("Part 1 Monkey Business:", inspectedItems[len(inspectedItems)-1]*inspectedItems[len(inspectedItems)-2])
+	fmt.Println("Part 1 Monkey Business:", inspectedItems_p1[len(inspectedItems_p1)-1]*inspectedItems_p1[len(inspectedItems_p1)-2])
 	fmt.Printf("Took %s\n", time.Since(start))
 }
