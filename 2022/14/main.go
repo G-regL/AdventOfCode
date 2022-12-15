d9e1741192c41b26f4e733f7113eecf6502b1594
Greg <Greg.Linton@gmail.com>
2022-12-14 23:02:56 -0500

    Cleaned up 2022/14

--- 2022/14/main.go
+++ 2022/14/main.go
@@ -11,6 +11,7 @@ import (
 )
 
 var flag_testData = flag.Bool("test", false, "Use Test dataset")
+var flag_Draw = flag.Bool("draw", false, "Draw out the blocks")
 
 type coord struct {
 	x, y int
@@ -34,24 +35,6 @@ func drawScan(points map[coord]string, maxY, minX, maxX int) {
 	}
 }
 
-func checkBelow(scan map[coord]string, p coord) bool {
-
-	u := coord{x: p.x, y: p.y + 1}
-	r := coord{x: p.x + 1, y: p.y + 1}
-	l := coord{x: p.x - 1, y: p.y + 1}
-	if _, occupied_u := scan[u]; occupied_u {
-		return true
-	} else {
-		if _, occupied_l := scan[l]; occupied_l {
-			return true
-		} else if _, occupied_r := scan[r]; !occupied_r {
-			return true
-		} else {
-			return false
-		}
-	}
-}
-
 func main() {
 	start := time.Now()
 	flag.Parse()
@@ -72,37 +55,29 @@ func main() {
 	sfile := string(bfile)
 
 	scan := map[coord]string{}
+	scanP1 := map[coord]string{}
 	biggestY := 0
 	smallestX, biggestX := 500, 500
 	scan[coord{x: 500, y: 0}] = "+"
 	for _, line := range strings.Split(sfile, "\n") {
-		//path := []string{}
-		//fmt.Println("line", line)
 
 		points := strings.Split(line, " -> ")
-		//fmt.Println("points", points)
 		last := coord{x: -1, y: -1}
 		for _, point := range points {
-			//fmt.Println("point", point)
 			p := strings.Split(string(point), ",")
-			//fmt.Println("p", p)
 			cur := coord{x: toInt(p[0]), y: toInt(p[1])}
 			scan[cur] = "#"
 			//moving up/down
 			if last.x == cur.x {
 				//down
 				if last.y > cur.y {
-					//fmt.Println("  last.y > cur.y  ", last.y, cur.y, last.y > cur.y)
 					for i := last.y; i >= cur.y; i-- {
 						scan[coord{x: cur.x, y: i}] = "#"
-						//fmt.Println("# at", coord{x: cur.x, y: i})
 					}
 					//up
 				} else if last.y < cur.y {
-					//fmt.Println("  last.y < cur.y  ", last.y, cur.y, last.y < cur.y)
 					for i := last.y; i <= cur.y; i++ {
 						scan[coord{x: cur.x, y: i}] = "#"
-						//fmt.Println("# at", coord{x: cur.x, y: i})
 					}
 				}
 			}
@@ -111,22 +86,17 @@ func main() {
 			if last.y == cur.y {
 				//left
 				if last.x > cur.x {
-					//fmt.Println("  last.x > cur.x  ", last.x, cur.x, last.x > cur.x)
 					for i := last.x; i >= cur.x; i-- {
 						scan[coord{x: i, y: cur.y}] = "#"
-						//fmt.Println("# at", coord{x: i, y: cur.y})
 					}
 					//right
 				} else if last.x < cur.x {
-					//fmt.Println("  last.x < cur.x  ", last.x, cur.x, last.x < cur.x)
 					for i := last.x; i <= cur.x; i++ {
 						scan[coord{x: i, y: cur.y}] = "#"
-						//fmt.Println("# at", coord{x: i, y: cur.y})
 					}
 				}
 			}
 			last = cur
-			//fmt.Println(scan)
 			if cur.y > biggestY {
 				biggestY = cur.y
 			}
@@ -140,46 +110,46 @@ func main() {
 		}
 	}
 
-	//fmt.Println(len(scan), biggestY)
+	minx, maxx := smallestX, biggestX
+	for f := smallestX - 200; f <= biggestX+200; f++ {
+		scan[coord{x: f, y: biggestY + 2}] = "#"
 
-	// minx, maxx := smallestX, biggestX
-	// for f := smallestX - 20; f <= biggestX+20; f++ {
-	// 	scan[coord{x: f, y: biggestY + 2}] = "#"
-
-	// 	if f < minx {
-	// 		minx = f
-	// 	}
-	// 	if f > maxx {
-	// 		maxx = f
-	// 	}
-	// }
-	// smallestX, biggestX = minx, maxx
+		if f < minx {
+			minx = f
+		}
+		if f > maxx {
+			maxx = f
+		}
+	}
+	smallestX, biggestX = minx, maxx
 
 	flowing := true
 	sandCounter := 0
 	sand_P1 := 0
-	//for loop := 1; loop <= 24 && flowing == true; loop++ {
+
+	//for loop := 1; loop <= 100 && flowing == true; loop++ {
+	//for loop := 1; loop <= 100; loop++ {
 	for flowing == true {
 
 		sand := coord{x: 500, y: 0}
 		rested := false
-		for flowing == true && rested == false {
+		for rested == false {
 
 			_, sand_bb := scan[coord{x: sand.x, y: sand.y + 1}]
 			_, sand_br := scan[coord{x: sand.x + 1, y: sand.y + 1}]
 			_, sand_bl := scan[coord{x: sand.x - 1, y: sand.y + 1}]
 
-			// rework loop to increment/decrement x, and increment y as needed
 			if !sand_bb {
 				sand.y++
-				if sand.y > biggestY+5 {
-					fmt.Println("overflow")
-					flowing = false
+				if sand.y > biggestY && sand_P1 == 0 {
+					//sand overflows the rock, and would keep going, so capture the number now for P1
 					sand_P1 = sandCounter
+					for k, v := range scan {
+						scanP1[k] = v
+					}
 				}
 			} else {
 
-				// find way to incrase X and loop again
 				if !sand_bl {
 					sand.x--
 				} else if !sand_br {
@@ -192,14 +162,22 @@ func main() {
 			//}
 		}
 		scan[coord{x: sand.x, y: sand.y}] = "o"
-		//fmt.Println(sandCounter)
+		if scan[coord{x: 500, y: 0}] == "o" {
+			flowing = false
+		}
 		sandCounter++
 	}
 
-	//drawScan(scan, biggestY+5, smallestX, biggestX)
+	if *flag_Draw {
+		fmt.Printf("\n-----------------------------------------------   Part 1   ---------------------------------\n")
+		drawScan(scanP1, biggestY+1, smallestX, biggestX)
+		fmt.Printf("\n-----------------------------------------------   Part 2   ---------------------------------\n")
+		drawScan(scan, biggestY+2, smallestX, biggestX)
+	}
 
 	// Show us what we've got!
 	fmt.Println("Part 1 Sand:", sand_P1)
+	fmt.Println("Part 2 Sand:", sandCounter)
 
 	fmt.Printf("Took %s\n", time.Since(start))
 
