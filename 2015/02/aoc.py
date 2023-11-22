884f9edca7857af5d53652d1caa08b9fe27b6090
Greg <Greg.Linton@gmail.com>
2023-11-22 16:20:27 -0500

    Got 2015/02/p2

--- 2015/02/aoc.py
+++ 2015/02/aoc.py
@@ -34,15 +34,14 @@ with open(ARG_data) as f:
     data = f.read()
 
 answer_p1 = 0
-#answer_p2 = None
+answer_p2 = 0
 for line in data.split('\n'):
 
-
-
     #Part 1
-    dimensions = line.split('x')
-    logger.debug(f"present dimensions: l=%d; w=%d; h=%d", int(dimensions[0]), int(dimensions[1]), int(dimensions[2]))
-    sides = [int(dimensions[0]) * int(dimensions[1]), int(dimensions[1]) * int(dimensions[2]), int(dimensions[2]) * int(dimensions[0])]
+    dimensions = list(map(lambda n: int(n), line.split('x')))
+
+    logger.debug(f"present dimensions: l=%d; w=%d; h=%d", dimensions[0], dimensions[1], dimensions[2])
+    sides = [dimensions[0] * dimensions[1], dimensions[1] * dimensions[2], dimensions[2] * dimensions[0]]
     logger.debug(f"present sides: lw=%d; wh=%d; hl=%d", sides[0], sides[1], sides[2])
     present_paper = (2 * sides[0]) + (2 * sides[1]) + (2 * sides[2])
     
@@ -50,10 +49,17 @@ for line in data.split('\n'):
     present_slack = sides[0]
 
     answer_p1 += present_paper + present_slack
-    logger.debug(f"this present needs %d square feet, plus %d slack = %d", present_paper, present_slack, (present_paper + present_slack))
+    logger.debug(f"Paper - %d square feet, plus %d slack = %d", present_paper, present_slack, (present_paper + present_slack))
+
+    #Part 2
+    dimensions.sort()
+    present_ribbon_wrap = 2 * dimensions[0] + 2 * dimensions[1] 
+    present_ribbon_bow = dimensions[0] * dimensions[1] * dimensions[2]
+    answer_p2 += present_ribbon_wrap + present_ribbon_bow
+    logger.debug(f"Ribbon - %d to wrap, plus %d for bow = %d", present_ribbon_wrap, present_ribbon_bow, (present_ribbon_wrap + present_ribbon_bow))
 
 
 
-print(f"__P1__ Total square feet:", answer_p1)
-#print(f"__P2__ We got to basement at step:", answer_p2)
+print(f"__P1__ Total square feet of paper:", answer_p1)
+print(f"__P2__ Total feet of ribbon:", answer_p2)
 #logger.info(line)
