cc13ebf281f37ee24c02f87a1b355cd5a643a03c
Greg <Greg.Linton@gmail.com>
2023-11-23 10:40:56 -0500

    Got 2015/03/p2
    I banged my head against this for way too long.
    Turns out I hadn't been re-assigning myposition back to the base position object, so each move had been based on a starting point of 0,0.

--- 2015/03/aoc.py
+++ 2015/03/aoc.py
@@ -24,7 +24,8 @@ logger.setLevel(ARG_logLevel)
 ch = logging.StreamHandler()
 ch.setLevel(ARG_logLevel)
 # create formatter
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s; %(message)s")
+#formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s; %(message)s")
+formatter = logging.Formatter("%(message)s")
 # add formatter to ch
 ch.setFormatter(formatter)
 # add ch to logger
@@ -40,43 +41,62 @@ for line in data.split('\n'):
     grid = [[0 for i in range(5000)] for j in range(5000)]
     position = [0,0]
     multiple_presents = set()
+
+    grid_p2 = [[0 for i in range(5000)] for j in range(5000)]
+    position_p2 = {"santa": [0,0], "robo": [0,0]}
+    multiple_presents_p2 = set()
+
+    grid[0][0] = 1
+    grid_p2[0][0] = 1
+    multiple_presents.add("0:0")
+    multiple_presents_p2.add("0:0")
+
     #Part 1
     for line_index, line_value in enumerate(line):
+        who = ("santa" if line_index % 2 != 1 else "robo")
+        myposition = position_p2[who]
+        #print("santa" if who != 1 else "robo")
         if line_value == ">": # Right
             position = [position[0] + 1, position[1]]
+            myposition = [myposition[0] + 1, myposition[1]]
+
         elif line_value == "<": # Left
             position = [position[0] - 1, position[1]]
+            myposition = [myposition[0] - 1, myposition[1]]
+
         elif line_value == "^": # Up
             position = [position[0], position[1] + 1]
+            myposition = [myposition[0], myposition[1] + 1]
+
         elif line_value == "v": # Down
             position = [position[0], position[1] - 1]
+            myposition = [myposition[0], myposition[1] - 1]
         
+
+
         grid[position[0]][position[1]] += 1
-        logger.debug(f"move %s, new pos: %d:%d, presents here: %d", line_value, position[0], position[1], grid[position[0]][position[1]])
+        logger.debug(f"p1 - move %s, new pos: %d:%d, presents here: %d", 
+                        line_value, position[0], position[1], grid[position[0]][position[1]])
 
         if grid[position[0]][position[1]] >= 1:
-            logger.debug(f"More than one present, adding %d:%d to set", position[0], position[1])
+            logger.debug(f"p1 - At least one present, adding %d:%d to set", position[0], position[1])
             multiple_presents.add(str(position[0]) + ":" + str(position[1]))
 
+
+
+        grid_p2[myposition[0]][myposition[1]] += 1
+        logger.debug(f"p2 - move %s, new %s pos: %d:%d, presents here: %d",
+                        line_value, who, myposition[0], myposition[1], grid_p2[myposition[0]][myposition[1]])
+        if grid_p2[myposition[0]][myposition[1]] >= 1:
+            logger.debug(f"p2 - At least one present, adding %d:%d to p2 set", myposition[0], myposition[1])
+            multiple_presents_p2.add(str(myposition[0]) + ":" + str(myposition[1]))
+        position_p2[who] = myposition
+
+
     answer_p1 = len(multiple_presents)
-
-    # dimensions = list(map(lambda n: int(n), line.split('x')))
-
-    # logger.debug(f"present dimensions: l=%d; w=%d; h=%d", dimensions[0], dimensions[1], dimensions[2])
-    # sides = [dimensions[0] * dimensions[1], dimensions[1] * dimensions[2], dimensions[2] * dimensions[0]]
-    # logger.debug(f"present sides: lw=%d; wh=%d; hl=%d", sides[0], sides[1], sides[2])
-    # present_paper = (2 * sides[0]) + (2 * sides[1]) + (2 * sides[2])
-    
-    # sides.sort()
-    # present_slack = sides[0]
-
-    # answer_p1 += present_paper + present_slack
-    # logger.debug(f"Paper - %d square feet, plus %d slack = %d", present_paper, present_slack, (present_paper + present_slack))
-
-
-
+    answer_p2 = len(multiple_presents_p2)
 
     print(f"__P1__ Houses with at least one present:", answer_p1)
-#print(f"__P2__ Total feet of ribbon:", answer_p2)
+    print(f"__P2__ Houses with at least one present:", answer_p2)
 
 print("Took {} seconds to run".format(time.process_time_ns() / 1000000000))
