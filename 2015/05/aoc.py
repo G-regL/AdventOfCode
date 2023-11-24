286bcfc002df3ccbc091865795dc02b8a897693f
Greg <Greg.Linton@gmail.com>
2023-11-23 21:48:03 -0500

    2015: Got 05/p2
    Learned a lot here, so this is really handy!

--- 2015/05/aoc.py
+++ 2015/05/aoc.py
@@ -29,8 +29,10 @@ logger.addHandler(ch)
 
 data = open(ARG_data).read()
 
+dataTestP2 = ['qjhvhtzxzqqjkmpb', 'xxyxx', 'uurcxstgmygtbstg', 'ieodomkazucvgmuy']
+
 answer_p1 = 0
-#answer_p2 = 0
+answer_p2 = 0
 
 nice_vowels = ['a', 'e', 'i', 'o', 'u']
 nice_doubles = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj', 'kk', 'll', 'mm', 
@@ -62,10 +64,46 @@ for niceString in data.split('\n'):
             foundNiceDouble = True
 
     if foundNiceVowel >= 3 and foundNiceDouble:
+        logger.debug(f'  %s is NICE!', niceString)
         answer_p1 += 1
+
+if ARG_data == 'test.txt':
+    data = "\n".join(dataTestP2)
+
+for niceString in data.split('\n'):
+
+    logger.debug(niceString)
+    
+
+    pairs = [niceString[i:i+2] for i in range(0, len(niceString), 2)] + \
+            [niceString[i:i+2] for i in range(1, len(niceString) -1, 2)]
+    pairs = list(filter(lambda x: len(x) == 2, pairs))
+    logger.debug(f"  {pairs}")
+
+    repeats = [niceString[i:i+3] for i in range(0, len(niceString), 3)] + \
+                [niceString[i:i+3] for i in range(1, len(niceString), 3)] + \
+                [niceString[i:i+3] for i in range(2, len(niceString), 3)]
+    repeats = list(filter(lambda x: len(x) == 3, repeats))
+    logger.debug(f"  {repeats}")
+
+    foundP2Pair = False
+    foundP2Repeat = False
+    for p in pairs:
+        if len(p) == 2 and niceString.count(p) >= 2:
+            logger.debug("  Found Nice pair %s", p)
+            foundP2Pair = True
+    
+    for r in repeats:
+        if len(r) == 3 and r[0] == r[2] and r[0] != r[1]:
+            logger.debug("  Found Nice repeat %s", r)
+            foundP2Repeat = True
+
+    if foundP2Pair and foundP2Repeat:
+        logger.debug(f'  %s is NICE!', niceString)
+        answer_p2 += 1
 
 
 print(f"__P1__ Nice strings:", answer_p1)
-    #print(f"__P2__ Number is:", answer_p2)
+print(f"__P2__ Nice strings:", answer_p2)
 
 print("Took {} seconds to run".format(time.process_time_ns() / 1000000000))
