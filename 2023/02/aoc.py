c0f5a6c905fc3eeae17c07a7dc350b291a9833dd
Greg <Greg.Linton@gmail.com>
2023-12-02 09:22:00 -0500

    2023: Got 02/p1

--- 2023/02/aoc.py
+++ 2023/02/aoc.py
@@ -25,10 +25,42 @@ data = open(ARG_data).read().split('\n')
 
 
 for line in data:
-   
+    game, cube_sets = line.split(":")
+    game = int(game.replace("Game ", ''))
+
+    cube_sets = cube_sets.strip().split(';')
+
+    debug(f"Game {game} - {cube_sets}")
+    game_possible = True
+    for pull in cube_sets:
+        #pull = pulls.split(';')
+
+        pull = pull.strip().split(',')
+        debug(f'  pull - {pull}')
+
+        red = green = blue = 0
+        for cube in pull:
+            count, colour = cube.strip().split(' ')
+            debug(f'    cube - count:{count}, colour:{colour}')
+            if colour == "red":
+                red = int (count)
+            elif colour == "green":
+                green = int(count)
+            elif colour == "blue":
+                blue = int(count)
+    
+        debug(f"    cubes - r{red}.g{green}.b{blue}")
+        if red > 12 or green > 13 or blue > 14:
+            debug(f'      Found impossible pull')
+            game_possible = False
+                
+
+    if game_possible:
+        debug(f'  Game Possible!!')
+        answer_p1 += game
 
 
-print(f"__P1__ Sum of calibration values: {answer_p1}")
+print(f"__P1__ Sum of possible games: {answer_p1}")
 print(f"__P2__ Sum of calibration values: {answer_p2}")
 #logger.info(line)
 
