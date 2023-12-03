511500d14aaf5362b4e5b15972d1d261b0342001
Greg <Greg.Linton@gmail.com>
2023-12-03 10:18:40 -0500

    2023: Got 03/p1
    Helps to actually have all the right adjacent coordinate deltas eh?

--- 2023/03/aoc.py
+++ 2023/03/aoc.py
@@ -22,14 +22,71 @@ answer_p1 = 0
 answer_p2 = 0
 
 # read input data and split into list of lines
-data = open(ARG_data).read().split('\n')
+grid = open(ARG_data).read().split('\n')
 
+x_limit = [0,len(grid[0])]
+y_limit = [0,len(grid)]
+
+debug(f' foundn x_limits {x_limit}, and y_limits {y_limit}')
+adjacent_delta_coords = [[0,-1], [1,-1], [1, 0], [1,1], [0, 1], [-1,1], [-1,0], [-1,-1]]
+
+part_numbers = []
+number = ""
+number_touching_symbol = False
 # loop through lines
-for line in data:
-    pass
+for y,line in enumerate(grid):
+    for x,char in enumerate(line):
 
+        #debug(f"char {char} at {x},{y}")
+        # We don't care about non digits in our grid
+        if not char.isdigit():
+
+            if number_touching_symbol:
+                debug(f"Adding {number} to parts list")
+                part_numbers.append(int(number))
+            #elif char == ".":
+                #debug(f"  igoring because is .")
+            #else:
+            #    debug(f"{number} not ajdacent to symbol")
+
+            number = ""
+            number_touching_symbol = False
+            continue
+
+
+        #Main detection loop
+        if char.isdigit():
+            number += char
+            for adj in adjacent_delta_coords:
+                #if the adjacent character is outside the grid, we don't do anything
+                if not (x_limit[0] < x + adj[0] < x_limit[1]) or not (y_limit[0] < y + adj[1] < y_limit[1]):
+                    #debug(f"  {x + adj[0]},{y + adj[1]} is out of bounds")
+                    continue
+
+                adj_char = grid[y + adj[1]][x + adj[0]]
+                #debug(f'  {adj_char} at {x + adj[0]},{y + adj[1]} ({adj[0]},{adj[1]}) is adjacent')
+                #debug(f"    in bounds")
+                #If the adjacent character ia a . or another digit, go to the next adjacent character
+                if adj_char == "." or adj_char.isdigit():
+                    #debug(f'    is . or digit')
+                    continue
+
+                #debug(f'    not . or digit')
+
+                #debug(f"    has to be a symbol, right?")
+                number_touching_symbol = True
+            
+
+        
+
+
+
+
+
+
+debug(f" Parts list: {part_numbers}")
 # Print out the answers
-print(f"__P1__ Sum of part numbers: {answer_p1}")
+print(f"__P1__ Sum of part numbers: {sum(part_numbers)}")
 #print(f"__P2__ Sum of set powers: {answer_p2}")
 
 # Tell me how inefficecient my code is
