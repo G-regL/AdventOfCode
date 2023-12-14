0518907fcbab3f825538cd397c3d70190f46ac03
Greg <Greg.Linton@gmail.com>
2023-12-13 23:07:42 -0500

    2023: Got d11/p2
    Had a little help, but was most of the way there with it.
    Ended up having to calculate the offsets for the galaxies, versus actually adding the empty rows.

--- 2023/11/aoc.py
+++ 2023/11/aoc.py
@@ -22,12 +22,58 @@ answer_p1 = 0
 answer_p2 = 0
 
 # read input data and split into list of lines
-data = open(ARG_data).read().split('\n')
+grid = open(ARG_data).read().split('\n')
 
+galaxies = []
+
+#        x  y
+empty = [[],[]]
 
 # loop through data
-for line in data:
-    pass
+for y, line in enumerate(grid):
+    if line.count("#") == 0:
+        empty[1].append(y)
+    #grid.append(line)
+
+# Spin the gride 90 degres clock-wise
+spun = list(zip(*grid[::-1]))
+
+for x, line in enumerate(spun):
+    if line.count("#") == 0:
+        empty[0].append(x)
+
+galaxies_x_p1 = []
+galaxies_y_p1 = []
+
+expansion = 1000000
+galaxies_x_p2 = []
+galaxies_y_p2 = []
+for y, line in enumerate(grid):
+    for x, char in enumerate(line):
+        if char == "#":
+            galaxies_x_p1.append(x + sum(1 for i in empty[0] if i < x) * (2 - 1))
+            galaxies_y_p1.append(y + sum(1 for i in empty[1] if i < y) * (2 - 1))
+            galaxies_x_p2.append(x + sum(1 for i in empty[0] if i < x) * (expansion - 1))
+            galaxies_y_p2.append(y + sum(1 for i in empty[1] if i < y) * (expansion - 1))
+
+
+def distancesum (arr, n):
+    # sorting the array.
+    arr.sort()
+    # for each point, finding 
+    # the distance.
+    res = 0
+    sum = 0
+    for i in range(n):
+        res += (arr[i] * i - sum)
+        sum += arr[i]
+    return res
+
+def totaldistancesum( x , y , n ):
+    return distancesum(x, n) + distancesum(y, n)
+
+answer_p1 = totaldistancesum(galaxies_x_p1, galaxies_y_p1, len(galaxies_x_p1))
+answer_p2 = totaldistancesum(galaxies_x_p2, galaxies_y_p2, len(galaxies_x_p2))
 
 
 # Print out the answers
