cd8b0977f946d064c36171b1fa603fab44fc2d34
Greg Linton <Greg.Linton@gmail.com>
2024-12-13 14:51:39 -0500

    2024: Got d13!
    CanChat helped me with the math, but the implementation is all mine..

--- 2024/13/aoc.py
+++ 2024/13/aoc.py
@@ -23,29 +23,63 @@ answer_p1 = 0
 answer_p2 = 0
 
 #INPUT
-# #Generate single list of lines
-# data = open(ARG_data).read().split('\n')
+import re
+from math import gcd
+machines_raw = open(ARG_data).read().split('\n\n')
 
-# #Generate a 2d matrix for a grid, including 
-# #In/out of range checks are done with in_grid(row, column)
-# grid = [list(map(int, list(row))) for row in data]
-# limits = (len(grid[0]), len(grid))
-# def in_grid(row, col) -> bool:
-#     global limits
-#     return 0 <= row < limits[0] and 0 <= col < limits[1]
+machines = []
+for mr in machines_raw:
+    a, b, p = mr.split('\n')
 
-# #Same, but using a dictionary, with keys being (row, column) tuples
-# #Makes in/out of range checks dead simple, using grid.get((r,c))
-# #    None if it doesn't exist, value otherwise
-# grid = {(r,c): int(char) for r, row in enumerate(data) for c, char in enumerate(row)}
+    a = re.match(r'Button A: X\+(\d+), Y\+(\d+)', a).groups()
+    b = re.match(r'Button B: X\+(\d+), Y\+(\d+)', b).groups()
+    p = re.match(r'Prize: X=(\d+), Y=(\d+)', p).groups()
+    m = {
+        "a": tuple(map(int, a)),
+        "b": tuple(map(int, b)),
+        "p": tuple(map(int, p)),
+    }
+    m["p2"] = (m["p"][0] + 10000000000000, m["p"][1] + 10000000000000)
 
+    machines.append(m)
 
-# Do things!!
+for m in machines:
+    ax = m["a"][0] * m["a"][1]
+    ay = m["b"][0] * m["a"][1]
+    ap = m["p"][0] * m["a"][1]
+    ap2 = (m["p2"][0]) * m["a"][1]
+    #print(ax, ay, ap)
+
+    bx = m["a"][1] * m["a"][0]
+    by = m["b"][1] * m["a"][0]
+    bp = m["p"][1] * m["a"][0]
+    bp2 = (m["p2"][1]) * m["a"][0]
+    #print(bx,by, bp)
+
+    #print(by - ay, bp - ap)
+
+    a_presses = (bp - ap) / (by - ay)
+    a2_presses = (bp2 - ap2) / (by - ay)
+
+    #print(f"{a_presses=}, {a_presses.is_integer()=}")
+
+    bx_a = m["b"][0] * a_presses
+    bx_a2 = m["b"][0] * a2_presses
+    #print(m["b"][0], bx_a, m["p"][0])
+    b_presses = (m["p"][0] - bx_a) / m["a"][0]
+    b2_presses = (m["p2"][0] - bx_a2) / m["a"][0]
+    #print(f"{b_presses=}, {b_presses.is_integer()=}")
+
+    if a_presses.is_integer() and b_presses.is_integer():
+        answer_p1 += b_presses * 3 + a_presses
+
+    if a2_presses.is_integer() and b2_presses.is_integer():
+        answer_p2 += b2_presses * 3 + a2_presses
 
 
 # Print out the answers
-print(f"\33[32m__P1__: \33[1m{answer_p1}\33[0m")
-print(f"\33[32m__P2__: \33[1m{answer_p2}\33[0m")
+print(f"\33[32m__P1__: \33[1m{int(answer_p1)}\33[0m")
+print(f"\33[32m__P2__: \33[1m{int(answer_p2)}\33[0m")
 
 # Tell me how inefficecient my code is
 print(f"\33[35mTook \33[1;35m{time.process_time_ns() / 1000000000}\33[0m\33[35m seconds to run\33[0m")
