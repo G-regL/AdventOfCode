3f3b007296d9e2e796869b30ced354977ee23e55
Greg <Greg.Linton@gmail.com>
2025-12-05 15:39:49 -0500

    2025: Got d5p2
    Had some help.. Ok lots of help, but it turned out to be easier than I thought.

--- 2025/05/aoc.py
+++ 2025/05/aoc.py
@@ -1,45 +1,64 @@
-import sys, getopt
+import getopt
+import sys
 import time
 
 # By default I want the real input, and *not* to debug.
-ARG_data = 'input.txt'
+ARG_data = "input.txt"
 ARG_debugLogging = False
 
 # Get the args off the command-line
-opts, args = getopt.getopt(sys.argv[1:],"td",["test","debug"])
+opts, args = getopt.getopt(sys.argv[1:], "td", ["test", "debug"])
 for opt, arg in opts:
     if opt in ("-t", "--test"):
-        print(f"\33[31mUSING TEST DATA\33[0m")
-        ARG_data = 'test.txt'
+        print("\33[31mUSING TEST DATA\33[0m")
+        ARG_data = "test.txt"
     elif opt in ("-d", "--debug"):
         ARG_debugLogging = True
+
 
 # Useful during testing to see what the hell is going on
 def debug(thing):
     if ARG_debugLogging:
         print(f"\33[39mDEBUG:\33[0m {thing}")
 
+
 answer_p1 = 0
 answer_p2 = 0
 
-#INPUT
-fresh_raw, available_raw = open('input.txt').read().split('\n\n')
+# INPUT
+fresh_raw, available_raw = open("input.txt").read().split("\n\n")
 
 fresh_ranges = [list(map(int, item.split("-"))) for item in fresh_raw.split("\n")]
 available = [int(item) for item in available_raw.split("\n")]
 
 # Do things!!
+# Part 1
 fresh_ingredients = set()
 for a in available:
-    for s,e in fresh_ranges:
+    for s, e in fresh_ranges:
         if s <= a <= e:
             fresh_ingredients.add(a)
 
 answer_p1 = len(fresh_ingredients)
+
+# Part 2
+fresh_ranges = sorted(fresh_ranges, key=lambda x: x[0])
+known_ranges = [fresh_ranges[0]]
+for fs, fe in fresh_ranges[1:]:
+    if fs > known_ranges[-1][1]:
+        known_ranges.append([fs, fe])
+    else:
+        # otherwise, extend the last range
+        known_ranges[-1][1] = max(known_ranges[-1][1], fe)
+
+answer_p2: int = sum([e - s + 1 for s, e in known_ranges])
+
 
 # Print out the answers
 print(f"\33[32m__P1__: \33[1m{answer_p1}\33[0m")
 print(f"\33[32m__P2__: \33[1m{answer_p2}\33[0m")
 
 # Tell me how inefficecient my code is
-print(f"\33[35mTook \33[1;35m{time.process_time_ns() / 1000000000}\33[0m\33[35m seconds to run\33[0m")
+print(
+    f"\33[35mTook \33[1;35m{time.process_time_ns() / 1000000000}\33[0m\33[35m seconds to run\33[0m"
+)
